3b01b280570c0704f80867c932c0428f7c4a96ca
iftachsc <iftachsc@cloudzone.io>
2021-10-16 20:41:17 +0300

    added deletion of topic

--- kafka.js
+++ kafka.js
@@ -1,10 +1,13 @@
 const { Kafka } = require('kafkajs')
+const { CompressionTypes, CompressionCodecs } = require('kafkajs')
+const LZ4 = require('kafkajs-lz4')
 
 const { KAFKA_USERNAME: username, KAFKA_PASSWORD: password } = process.env
 
 //const sasl = username && password ? { username, password, mechanism: 'plain' } : null
 //const ssl = !!sasl
 
+CompressionCodecs[CompressionTypes.LZ4] = new LZ4().codec
 
 // This creates a client instance that is configured to connect to the Kafka broker provided by
 // the environment variable KAFKA_BOOTSTRAP_SERVER
